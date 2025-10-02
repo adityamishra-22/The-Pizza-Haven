7ad30cb4101ac17165e462d9122547940660ca27
adityamishra22 <adi22mishra@gmail.com>
2025-10-02 12:14:36 +0530

    Update Geolocation

--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -1,9 +1,12 @@
 export async function getAddress({ latitude, longitude }) {
   const res = await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
   );
-  if (!res.ok) throw Error('Failed getting address');
+
+  if (!res.ok) {
+    throw new Error('Failed getting address');
+  }
 
   const data = await res.json();
-  return data;
+  return data; // contains locality, city, postcode, countryName etc.
 }
